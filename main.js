4b7746dc62a0366de3d32b04713e429162d18943
jtech <devresearcher24@gmail.com>
2022-11-22 04:05:26 +0200

    sketching with black

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+let color = "black";
+
 document.addEventListener("DOMContentLoaded", function(){
- createBoard(16)
- console.log("hey")
+ createBoard(16);
+ let btn_popup = document.querySelector("#popup");
+ btn_popup.addEventListener("click", function(){
+    let size = getSize();
+    createBoard(size);
+ })
+
 })
 
 function createBoard(size){
@@ -13,8 +20,37 @@ function createBoard(size){
 
     for(let i = 0; i < numDivs; i++){
         let div = document.createElement("div");
-        div.style.backgroundColor = "yellow";
+        div.addEventListener("mouseover", colorDiv)
+
         board.insertAdjacentElement("beforeend", div);
 
     }
+}
+ 
+function getSize(){
+    let input = prompt("what will be the size of the Board");
+    let message =document.querySelector("#message");
+    if (input==""){
+        message.innerHTML = "Please provide a number";
+    }
+    else if(input < 1 || input > 100){
+        message.innerHTML = "Please enter a number between 1 and 100";
+    }
+    else{
+        message.innerHTML = "Now you can play";
+        return input;
+    }
+
+}
+function colorDiv(){
+if(color == "random"){
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100, 50%)`;
+}
+else{
+    this.style.backgroundColor = 'black';
+}
+}
+function setColor(colorChoice){
+     color = colorChoice;
+
 }
